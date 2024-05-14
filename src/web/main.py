c3debb19272797c1447713c42cf3518e8cd3799e
hibana2077 <hibana2077@gmail.com>
2024-05-14 15:12:30 +0800

    chore: Remove unused files and directories in web app

--- src/web/main.py
+++ src/web/main.py
@@ -1,3 +1,11 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2024-05-05 11:49:31
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-05-14 15:05:52
+FilePath: \srcc:\Users\USER\Bottles-and-Cans-classifier\src\web\main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import streamlit as st
 from PIL import Image
 import requests
